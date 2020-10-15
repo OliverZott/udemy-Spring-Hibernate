b151f272cd3eb5429f8acdab47ea628c0a2b106b
OliverZott <zott_oliver@web.de>
2020-10-15 19:29:14 +0200

    version checker

---
+++ spring-hibernate-demo/src/VersionChecker.java
@@ -0,0 +1,7 @@
+import org.springframework.core.SpringVersion;
+
+public class VersionChecker {
+    public static void main(String[] args) {
+        System.out.println("version: " + SpringVersion.getVersion());
+    }
+}
