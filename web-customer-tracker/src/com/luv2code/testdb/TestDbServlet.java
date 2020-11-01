9fd371910c06aa191d284bf924549f5bd7c8c5be
OliverZott <zott_oliver@web.de>
2020-11-01 20:20:32 +0100

    Alternative non-servlet version of db-connection test due to IDE issues with eclipse project

--- web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -1,27 +1,31 @@
+/*
 package com.luv2code.testdb;
-
-import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-import java.sql.*;
-
+*/
 /**
  * Servlet implementation class TestDbServlet
- */
+ *//*
+
 @WebServlet("/TestDbServlet")
 public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
+	*/
+/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
-	 */
+	 *//*
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -54,3 +58,4 @@ public class TestDbServlet extends HttpServlet {
 	}
 
 }
+*/
